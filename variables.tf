99109b81d5d53f617a6d6f79fc5a6dcc1e4d520c
susmitha48 <94097370+susmitha48@users.noreply.github.com>
2023-02-28 21:20:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 
 variable "AWS_ACCESS_KEY" {
-    default = "AKIAREOUR4ZP5C4LNMAK"
+    default = ""
 }
 variable "AWS_SECRET_KEY" {
-    default = "HXGtxH0606qLTTxza6L2w1AWLibubdQ+S1t56KmN"
+    default = ""
 }
