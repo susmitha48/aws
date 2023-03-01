a01a0f7f2ddad010776c5fa5466f3ccff249e052
susmitha48 <94097370+susmitha48@users.noreply.github.com>
2023-03-01 14:51:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,24 +5,24 @@ terraform {
       version = "3.74.2"
     }
   }
-  cloud {
+  /*cloud {
     organization = "shining"
     workspaces {
       name = "aws"
     }
-  }
+  }*/
 } 
 provider "aws" {
   region = "ap-south-1"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
-/*terraform {
+terraform {
   backend "s3" {
     bucket = "cf-sogeti-s3-us-east-1-tfbackend"
     key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "tfstateforgithubactions"
-    profile = ""
+    profile = "githubactions"
   }
-}*/
+}
