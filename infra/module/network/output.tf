a0ef9d984ad4570bcf319a9cd25ac88817ebc602
susmitha48 <susmitha.tatikonda@capgemini.com>
2023-03-02 18:56:39 +0530

    modified the output.tf

--- infra/module/network/output.tf
+++ infra/module/network/output.tf
@@ -3,5 +3,5 @@ output "internet_gateway_id" {
   description = "The internet gateway for vpc has been created"
 }
 output "transit_gateway" {
-    value= module.transit_gateway.aws_ec2_transit_gateway  
+    value= module.transit_gateway.*.aws_transit_gateway  
 }
